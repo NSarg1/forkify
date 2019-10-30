43350e8828124d1ebba703a890f0b647a6010472
NSarg7 <NSarg7@users.noreply.github.com>
2019-10-30 20:24:29 +0400

    Built the Search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,34 @@
-// Global app controller
+import Search from './models/Search';
+
+/** Global state app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Linked recipes
+ */
+const state = {}
+
+const controlSearch = async () => {
+    // Get query from view
+    const query = 'pizza' //ToDo
+
+    if (query) {
+        // 2. New Search object
+        state.search = new Search(query)
+        // 3. Prepare UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.results);
+    }
+}
+
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+
+
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+export default class Search{
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = 'd56d9cc36979e1a8a889e08df600fcb4'
+        try{
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch(err){
+            alert(err)
+        }        
+    }
+
+
+
+
+
+}
